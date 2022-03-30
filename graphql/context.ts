fa6076c4ecc50c5eab1e7a88c6ae31416f2510d3
Your Name <mamsheikh123@gmail.com>
2022-03-30 20:12:03 +0100

    🎉 initial commit - setup

---
+++ graphql/context.ts
@@ -0,0 +1,29 @@
+import { NextApiResponse, NextApiRequest } from 'next'
+import { PrismaClient } from '@prisma/client'
+import prisma from '../lib/prisma'
+
+export type Context = {
+  prisma: PrismaClient
+  req: NextApiRequest
+  res: NextApiResponse
+  // email: string;
+  // user: User;
+}
+// const user = await prisma.user.findUnique({
+//   where:{email:session.user.email}
+// })
+export async function createContext({
+  req,
+  res,
+}: {
+  req: NextApiRequest
+  res: NextApiResponse
+}): Promise<Context> {
+  return {
+    prisma,
+    req,
+    res,
+    // email: session.user.email,
+    // user,
+  }
+}
